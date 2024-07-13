9bec0d0734e6f8982695893f35e43201ee42ba42
Shreemirrah2101 <shreemirraha@docu3c.com>
2024-07-13 08:02:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from llama_index.core.prompts import PromptTemplate
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core import get_response_synthesizer
+
 import streamlit as st
 import logging
 import sys
@@ -24,11 +25,9 @@ api_version = "2023-03-15-preview"
 
 
 connect_str = 'DefaultEndpointsProtocol=https;AccountName=shreemirrahrag;AccountKey=UlbcKqbBtcs0J+2esmr8AznpwdV1dF5XI+v13kY07sjJ2U8rHmWbtCUqLjb12lD7Bn9k17mWgqjd+AStB4xXcw==;EndpointSuffix=core.windows.net'
-
-blob_service_client_1 = BlobServiceClient(account_url='https://shreemirrahrag.blob.core.windows.net/document1', credentials='UlbcKqbBtcs0J+2esmr8AznpwdV1dF5XI+v13kY07sjJ2U8rHmWbtCUqLjb12lD7Bn9k17mWgqjd+AStB4xXcw==')
-blob_service_client_2 = BlobServiceClient(account_url='https://shreemirrahrag.blob.core.windows.net/document2', credentials='UlbcKqbBtcs0J+2esmr8AznpwdV1dF5XI+v13kY07sjJ2U8rHmWbtCUqLjb12lD7Bn9k17mWgqjd+AStB4xXcw==')
-blob_service_client_3 = BlobServiceClient(account_url='https://shreemirrahrag.blob.core.windows.net/document3', credentials='UlbcKqbBtcs0J+2esmr8AznpwdV1dF5XI+v13kY07sjJ2U8rHmWbtCUqLjb12lD7Bn9k17mWgqjd+AStB4xXcw==')
-blob_service_client_4 = BlobServiceClient(account_url='https://shreemirrahrag.blob.core.windows.net/document4', credentials='UlbcKqbBtcs0J+2esmr8AznpwdV1dF5XI+v13kY07sjJ2U8rHmWbtCUqLjb12lD7Bn9k17mWgqjd+AStB4xXcw==')
+container_name = 'docs'
+account_url='https://shreemirrahrag.blob.core.windows.net/docs'
+blob_service_client = BlobServiceClient(account_url=account_url, credentials='UlbcKqbBtcs0J+2esmr8AznpwdV1dF5XI+v13kY07sjJ2U8rHmWbtCUqLjb12lD7Bn9k17mWgqjd+AStB4xXcw==')
 
 llm = AzureOpenAI(
     model="gpt-35-turbo-16k",
@@ -48,43 +47,18 @@ embed_model = AzureOpenAIEmbedding(
 
 Settings.llm = llm
 Settings.embed_model = embed_model
+Settings.chunk_size = 1000
 
-vector_store_1 = PGVectorStore.from_params(
+vector_store = PGVectorStore.from_params(
     database='citus',
     host='c-shreemirrah-final.6c2zuyv2zdqcrp.postgres.cosmos.azure.com',
     password='Admin123!',
     port=5432,
     user='citus',
-    table_name="rag_document1",
+    table_name="rag_v1",
     embed_dim=1536)
 
-vector_store_2 = PGVectorStore.from_params(
-    database='citus',
-    host='c-shreemirrah-final.6c2zuyv2zdqcrp.postgres.cosmos.azure.com',
-    password='Admin123!',
-    port=5432,
-    user='citus',
-    table_name="rag_document2_correct_v1",
-    embed_dim=1536)
-
-vector_store_3 = PGVectorStore.from_params(
-    database='citus',
-    host='c-shreemirrah-final.6c2zuyv2zdqcrp.postgres.cosmos.azure.com',
-    password='Admin123!',
-    port=5432,
-    user='citus',
-    table_name="rag_document3",
-    embed_dim=1536)
-
-vector_store_4 = PGVectorStore.from_params(
-    database='citus',
-    host='c-shreemirrah-final.6c2zuyv2zdqcrp.postgres.cosmos.azure.com',
-    password='Admin123!',
-    port=5432,
-    user='citus',
-    table_name="rag_document4",
-    embed_dim=1536)
-
+index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
 context_str="""You're an assistant who fetches answers from the documents. Add all the necessary details."""
 
@@ -94,10 +68,7 @@ particularly from documented sources.
 Your goal is to provide accurate, concise, and contextually relevant answers based on the given information.
 
 Instructions:
-Parties Involved: when referring to the parties involved, find the 'specific names' of the parties along with their role and not just their role. For example, don't refer to parties by just 'Licensee' use '<company name> (Licensee)'
-Addresses: Addresses are crucial when it comes to documents. Hence, keep a track of addresses and the companies which they correspond to and retreive them accurately. Even if the term 'address' is not explicitly mentioned, addresses are of the format: '<Number> Place Street State-Code(Eg: CA, PA etc), 5-digit Pincode' Eg: 40 Pacifica, Suite 900 Trvine, CA 92618 
-Dates: When it comes to retirieving information pertaining to dates in the document, look into the context associated with the dates. For example, you might have to retreive the date when the agreement is signed, In this case you'd have to look for the date that's associated with signature(Look for places such as By: <company-name> Title:<title> Date:date of signature). In other cases, for example, you'd have to find the effective date for the agreement. In such cases you'd have find the dates that correspond to this keyword 'effective'. Use these clues to correctly return the associated dates
-Date of signature:(Look for places such as By: <company-name> Title:<title> Date:date of signature). signature dayes are mostly of the fomrat: __/__/____
+Dates: When it comes to retirieving information pertaining to dates in the document(when there's discrepency), give preference to choosing the dates that are more recent, the date with the day.month, and year as well as the ones which occur more frequently in the relevant documents
 Comprehension and Accuracy: Carefully read and comprehend the provided context from the documents to ensure accuracy in your response.
 Truthfulness: If the context does not provide enough information to answer the question, clearly state, "I don't know."
 Contextual Relevance: Ensure your answer is well-supported by the retrieved context and does not include any information beyond what is provided.
@@ -107,54 +78,39 @@ Here is the question and context for you to work with:
 
 \nQuestion: {question} \nContext: {context} \nAnswer:"""
 
+
 prompt_tmpl = PromptTemplate(
     template=template,
     template_var_mappings={"query_str": "question", "context_str": "context"},
 )
+retriever = VectorIndexRetriever(
+    index=index,
+    similarity_top_k=5
+)
 
-
-rerank = SentenceTransformerRerank(top_n = 5, model = "BAAI/bge-reranker-base"    )
+rerank = SentenceTransformerRerank(
+    top_n = 5, 
+    model = "BAAI/bge-reranker-base"
+)
 postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
 
 response_synthesizer = get_response_synthesizer()
 
+query_engine = RetrieverQueryEngine(
+    retriever=retriever,
+    response_synthesizer=response_synthesizer,node_postprocessors=[rerank,postproc]
+)
+
+query_engine.update_prompts(
+    {"response_synthesizer:text_qa_template":prompt_tmpl}
+)
+
+# postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
+# rerank = SentenceTransformerRerank(top_n = 2,model = "BAAI/bge-reranker-base")
+
+# query_engine = index.as_query_engine(similarity_top_k = 6, alpha=0.5,node_postprocessors = [postproc, rerank],)
+
 def return_response(question):
-    st.markdown('Document 1: BPP_Bright final agreement_SPB\n')
-    index = VectorStoreIndex.from_vector_store(vector_store=vector_store_1)
-    retriever = VectorIndexRetriever(index=index,similarity_top_k=5)
-
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer,node_postprocessors=[rerank,postproc])
-
-    query_engine.update_prompts(
-        {"response_synthesizer:text_qa_template":prompt_tmpl}
-    )
-
-    response = query_engine.query(question)
-    st.write(response.response)
-    st.write('\n\n'+'Source:\n\n')
-    source=response.metadata
-
-    for i in source.keys():
-        st.write('\nSource:  '+source[i]['document_title']+'\t')    
-        file=source[i]['file_name']
-        blob_client = blob_service_client_1.get_blob_client('document1', file)
-        blob_url = blob_client.url
-        blob_url=blob_url.replace('https://shreemirrahrag.blob.core.windows.net/document1/document1','https://shreemirrahrag.blob.core.windows.net/document1')
-        st.markdown(f"[{file}]({blob_url})"+"\tPage: "+source[i]['page_label']+'\n')
-    st.markdown('\n\nDocument 2: Bright CoreLogic Infonet Agreement\n')
-    index = VectorStoreIndex.from_vector_store(vector_store=vector_store_2)
-    retriever = VectorIndexRetriever(index=index,similarity_top_k=5)
-
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer,node_postprocessors=[rerank,postproc])
-
-    query_engine.update_prompts(
-        {"response_synthesizer:text_qa_template":prompt_tmpl}
-    )
-
     response = query_engine.query(question)
     answer=''
     st.write(response.response)
@@ -164,60 +120,13 @@ def return_response(question):
     for i in source.keys():
         st.write('\nSource:  '+source[i]['document_title']+'\t')    
         file=source[i]['file_name']
-        blob_client = blob_service_client_1.get_blob_client('document2', file)
+        blob_client = blob_service_client.get_blob_client(container_name, file)
         blob_url = blob_client.url
-        blob_url=blob_url.replace('https://shreemirrahrag.blob.core.windows.net/document2/document2','https://shreemirrahrag.blob.core.windows.net/document2')
-        st.markdown(f"[{file}]({blob_url})"+"\tPage: "+source[i]['page_label']+'\n')    
-
-    st.markdown('\n\nDocument 3: CC_Bright_Clear Capital Access and License Agreement\n')
-    index = VectorStoreIndex.from_vector_store(vector_store=vector_store_3)
-    retriever = VectorIndexRetriever(index=index,similarity_top_k=5)
-
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer,node_postprocessors=[rerank,postproc])
-
-    query_engine.update_prompts(
-        {"response_synthesizer:text_qa_template":prompt_tmpl}
-    )
-
-    response = query_engine.query(question)
-    st.write(response.response)
-    st.write('\n\n'+'Source:\n\n')
-    source=response.metadata
-
-    for i in source.keys():
-        st.write('\nSource:  '+source[i]['document_title']+'\t')    
-        file=source[i]['file_name']
-        blob_client = blob_service_client_1.get_blob_client('document3', file)
-        blob_url = blob_client.url
-        blob_url=blob_url.replace('https://shreemirrahrag.blob.core.windows.net/document3/document3','https://shreemirrahrag.blob.core.windows.net/document3')
+        blob_url=blob_url.replace('https://shreemirrahrag.blob.core.windows.net/docs/docs','https://shreemirrahrag.blob.core.windows.net/docs')
         st.markdown(f"[{file}]({blob_url})"+"\tPage: "+source[i]['page_label']+'\n')
 
-    st.markdown('\n\nDocument 4: Signed ShowingTime for the MLS Agreement with Bright MLS\n')
-    index = VectorStoreIndex.from_vector_store(vector_store=vector_store_4)
-    retriever = VectorIndexRetriever(index=index,similarity_top_k=5)
 
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer,node_postprocessors=[rerank,postproc])
 
-    query_engine.update_prompts(
-        {"response_synthesizer:text_qa_template":prompt_tmpl}
-    )
-
-    response = query_engine.query(question)
-    st.write(response.response)
-    st.write('\n\n'+'Source:\n\n')
-    source=response.metadata
-
-    for i in source.keys():
-        st.write('\nSource:  '+source[i]['document_title']+'\t')    
-        file=source[i]['file_name']
-        blob_client = blob_service_client_1.get_blob_client('document4', file)
-        blob_url = blob_client.url
-        blob_url=blob_url.replace('https://shreemirrahrag.blob.core.windows.net/document4/document4','https://shreemirrahrag.blob.core.windows.net/document4')
-        st.markdown(f"[{file}]({blob_url})"+"\tPage: "+source[i]['page_label']+'\n')
 
 st.set_page_config(page_title='RAG Questions Answered Extractor')
 st.header('RAG Questions Answered Extractor')
